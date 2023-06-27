c064b6cb0dad4e11594cbacab3151d5aa92577bb
Lussati Santos <lussatisantos10@gmail.com>
2023-06-27 08:40:40 +0100

    lista de categorias
    sort baseando na data

--- rotas/admin.js
+++ rotas/admin.js
@@ -17,7 +17,7 @@ router.get('/categorias', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    Categoria.find().then((categorias) => {
+    Categoria.find().sort({date: 'desc'}).then((categorias) => {
         res.render('/admin/categorias', {categorias: categorias})
     }).catch((err) => {
         req.flash('error_msg', 'Houve um erro ao listar as categorias')

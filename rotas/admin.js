1780aa552d4f096db5f7592c6ee8ec82492230ed
Lussati Santos <lussatisantos10@gmail.com>
2023-06-27 09:11:29 +0100

    editar categorias
    editar categorias terminada!

--- rotas/admin.js
+++ rotas/admin.js
@@ -68,4 +68,24 @@ router.get('/categorias/edit/:id', (req, res) => {
     })
 })
 
+router.post('/categorias/edit', (req, res) => {
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(() => {
+            req.flash('success_msg', 'Categoria editada com sucesso!')
+            res.redirect('/admin/categorias')
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro ao salvar a categoria')
+            res.redirect('/admin/categorias')
+        })
+
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao editar a categoria')
+        res.redirect('/admin/categorias')
+    })
+})
+
 module.exports = router
